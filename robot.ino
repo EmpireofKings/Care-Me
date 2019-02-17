ca6f18e1473f4113ee854deb26262bd06ed80708
Shyana Srikanthalingam <shyanasri@gmail.com>
2019-02-17 15:44:48 -0500

    Update robot.ino

--- robot.ino
+++ robot.ino
@@ -1,3 +1,5 @@
+#include<SPI.h>
+#include<RF24.h>
 #define enA 9
 #define in1 4
 #define in2 5
@@ -5,6 +7,9 @@
 #define in3 6
 #define in4 7
 
+// ce, csn pins
+RF24 radio(0, 13)
+ 
 void setup() {
  pinMode(enA, OUTPUT);
  pinMode(enB, OUTPUT);
@@ -12,17 +17,33 @@ void setup() {
  pinMode(in2, OUTPUT);
  pinMode(in3, OUTPUT);
  pinMode(in4, OUTPUT);
-  Serial.begin(9600);
-    Serial.print("hi");
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-   
+ Serial.begin(9600);
+ Serial.print("hi");
+ digitalWrite(in1, LOW);
+ digitalWrite(in2, LOW);
+ while (!Serial);
+  radio.begin();
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setChannel(0x76);
+  radio.openWritingPipe(0xF0F0F0F0E1LL);
+  const uint64_t pipe = 0xE8E8F0F0E1LL;
+  radio.openReadingPipe(1, pipe);
+  radio.enableDynamicPayloads();
+  radio.powerUp();
 }
 
 void loop() {
   //digitalWrite(enA, HIGH);
-    //digitalWrite(enB, HIGH);
-  Serial.print("driving?");
+  //digitalWrite(enB, HIGH);
+ radio.startListening() ;
+  Serial.println("Starting loop. Radio on.") ;
+  char receivedMessage[32] = {0} ;
+  if (radio.available()){
+    radio.read(receivedMessage, sizeof(receivedMessage));
+    Serial.println(receivedMessage + '0') ;
+    Serial.println("Turning off the radio.") ;
+    radio.stopListening() ;
+   Serial.print("driving?");
 
   digitalWrite(in1, HIGH);
    digitalWrite(in2, LOW);
@@ -36,6 +57,6 @@ void loop() {
     // Set Motor B backward
    // digitalWrite(in3, HIGH);
     //digitalWrite(in4, LOW);
-    
+  }  
 delay(1000);
 }
